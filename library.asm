eaf81936813bccb0ea6de048aa25d328f786eef6
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-11-09 17:49:59 +0100

    some cleanup

--- library.asm
+++ library.asm
@@ -1,23 +1,32 @@
+;This can be assembled with NASM, maybe YASM
 section .text
 bits 64
-;See func.asm.hpp and main.cpp for "how to call these functions" from C++ code
+;See library.hpp and main.cpp for "how to call these functions" from C++ code
+
+;Exported symbols for the resuting object file. These are then just labels below
+global asm_is_windows, compute_answer, sum_4_ints, increment_pointer_int64
 
 
-global asm_is_windows
-asm_is_windows
+;This function return true if the WIN32 macro was forwarded properly from CMake
+; The top-level cmake file of this project will define WIN32 if Windows was detected as a platform
+; We can use this for doing conditional assembly to deal with different ABIs
+;
+;		bool asm_is_windows()
+asm_is_windows:
 %ifdef WIN32
-mov rax, 1
+	mov rax, 1		; return bool(true)
 %else
-xor rax, rax
+	xor rax, rax	; return bool(false)
 %endif
-ret
+	ret
 
 ;This is just a function that return the number 42
 ;Return values are "whatever is in RAX" in x64
-global compute_answer
+;
+;		int compute_answer()
 compute_answer:
-mov rax, 101010b
-ret
+	mov rax, 101010b
+	ret
 
 ;The 4 first arguments are passed on by registers
 ;The registers used for this purpose on WIN32 are
@@ -30,32 +39,30 @@ ret
 ; RSI - 2nd arg
 ; RCX - 3rd arg
 ; RDX - 4th arg
-global sum_4_ints
+;
+;		int sum_4_ints(int, int, int, int)
 sum_4_ints:
 %ifdef WIN32
-mov rax, rcx
-add rax, rdx
-add rax, r8
-add rax, r9
+	mov rax, rcx
+	add rax, rdx
+	add rax, r8
+	add rax, r9
 %else
-mov rax, rdi
-add rax, rsi
-add rax, rdx
-add rax, rcx
+	mov rax, rdi
+	add rax, rsi
+	add rax, rdx
+	add rax, rcx
 %endif
-ret
-
+	ret
 
 ;pointers are naturally passed as arguments in the same way;
 ;the following procedure act as the following C code would do
-;void increment_pointer_int(int64_t* pointer) { *pointer++; }
-global increment_pointer_int64
+;
+;		void increment_pointer_int(int64_t* pointer) { *pointer++; }
 increment_pointer_int64:
 %ifdef WIN32
-add QWORD [rcx], 1		;DWORD  [address] means that we are accessing a 32bit value
+	add QWORD [rcx], 1		;QWORD [address] means that we are accessing a 64bit value
 %else
-add QWORD [rdi], 1
+	add QWORD [rdi], 1
 %endif
-ret
-
-
+	ret
--- library.hpp
+++ library.hpp
@@ -4,9 +4,9 @@
 #include <cstdint>
 extern "C"
 {
-#else
+#else //is pure C
 #include <stdint.h>
-#endif
+#endif //__cplusplus
 
 // This test the WIN32 macro
 bool asm_is_windows();
@@ -17,7 +17,7 @@ bool asm_is_windows();
 // file comments for info about how to access these args
 int compute_answer();
 int sum_4_ints(int, int, int, int); // this is a prototype, the arguments don't have names but you
-                                    // can put them there if you want :-)
+									// can put them there if you want :-)
 
 // Test to dereference a C pointer in assembly and mutate it's content
 void increment_pointer_int64(int64_t*);
@@ -25,4 +25,4 @@ void increment_pointer_int64(int64_t*);
 
 #ifdef __cplusplus
 } //End of extern "C" bloc
-#endif
+#endif //__cplusplus
